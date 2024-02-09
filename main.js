4593aa224f338356c2b131680bb762cda756ca2d
J.VICTOR <112410588+joaovictor-bar-oli@users.noreply.github.com>
2024-02-09 13:36:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function abrirpag(){
+    var nome = document.getElementById('name-input').value;
+    
+    // Verifique se o input está preenchido
+    if(nome.trim() !== '') {
+        window.location.href = "https://youtu.be/wd7JqXouRRA?si=WMBQ75ZC5Wozdr_X&t=43"
+    }
+    else{
+        alert("Por favor preencha os campos login,nome")
+    }
+
+}
